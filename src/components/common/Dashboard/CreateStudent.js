69b5d53ba9425fa9eb771e4d3fe870210b25ff1e
falahyusufhamza <falahyusufhamza@gmail.com>
2023-02-12 12:12:18 +0400

    Create student form added

---
+++ src/components/common/Dashboard/CreateStudent.js
@@ -0,0 +1,133 @@
+import { Button, Col, Form, Input, Modal, Row, Select } from 'antd'
+import React from 'react'
+import "./CreateStudent.css"
+
+const CreateStudent = ({
+    isVisible = false,
+    onClose = () => {}
+}) => {
+    const {Option} = Select;
+    const [form] = Form.useForm();
+    const formItemLayout = {
+        labelCol: {
+          xs: {
+            span: 24,
+          },
+          sm: {
+            span: 8,
+          },
+        },
+        wrapperCol: {
+          xs: {
+            span: 24,
+          },
+          sm: {
+            span: 16,
+          },
+        },
+      };
+  return (
+    <Modal
+        open={isVisible}
+        className="create-student-modal"
+        title={<div className='modal-header'>
+            <p className='modal-title'>Create a new student</p>
+        </div>}
+        onCancel={onClose}
+        children={<div className='contents'>
+            <Form
+                {...formItemLayout}
+                form={form}
+                name="register"
+                onFinish={(values) => console.log(values)}
+                initialValues={{
+                    residence: ['zhejiang', 'hangzhou', 'xihu'],
+                    prefix: '86',
+                }}
+                scrollToFirstError
+                >
+                    <Row style={{width: "100%"}} gutter={20}>
+                    <Col style={{width: "100%"}} span={12}>
+                    <Form.Item
+                    name="name"
+                    label="Full name"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Name is required!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    name="grno"
+                    label="GR No."
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter the GR Number!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    name="gender"
+                    label="Gender"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'Please select gender!',
+                    },
+                    ]}
+                >
+                    <Select placeholder="select your gender">
+                        <Option value="male">Male</Option>
+                        <Option value="female">Female</Option>
+                    </Select>
+                </Form.Item>
+                    </Col>
+                
+                <Col span={12}>
+                <Form.Item
+                    name="school"
+                    label="School"
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    name="location"
+                    label="Location"
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    name="contact"
+                    label="Phone Number"
+                >
+                    <Input/>
+                </Form.Item>
+                
+                </Col>
+                    </Row>
+                <Row justify={"end"} style={{width: "100%", marginTop: 20, paddingRight: 20}}>
+                        <Button onClick={onClose} type="secondary">
+                            Cancel
+                        </Button>
+                        <Button style={{marginLeft: 10}} type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                </Row>
+            </Form>
+        </div>}
+        width={800}
+        footer={null}
+    />
+  )
+}
+
+export default CreateStudent
--- src/components/common/Dashboard/StudentsTable.js
+++ src/components/common/Dashboard/StudentsTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Table} from 'antd';
+import "./StudentsTable.css";
 
 const StudentsTable = ({
     data = [],
@@ -9,36 +10,42 @@ const StudentsTable = ({
         {
             title: 'GR',
             dataIndex: 'grno',
+            fixed: "left",
+            width: "50px",
         },
         {
           title: 'Name',
           dataIndex: 'name',
-          render: (name, record) => <div onClick={() => onSelectStudent(record?.grno)}>{name}</div>
+          render: (name, record) => <div className='student-name' onClick={() => onSelectStudent(record?.grno)}>{name}</div>,
+          fixed: "left",
+          width: "250px",
         },
         {
           title: 'School',
           dataIndex: 'school',
+          width: "200px"
         },
-        {
-          title: 'Class',
-          dataIndex: 'class',
-        },
-        {
-            title: 'Location',
-            dataIndex: 'location',
-        },
+        // {
+        //   title: 'Class',
+        //   dataIndex: 'class',
+        // },
+        // {
+        //     title: 'Location',
+        //     dataIndex: 'location',
+        // },
         {
             title: 'Bus No.',
             dataIndex: 'busNumber',
+            width: '100px'
         },
-        {
-            title: 'Father',
-            dataIndex: 'fatherName',
-        },
-        {
-            title: 'Contact number',
-            dataIndex: 'contactNo',
-        },
+        // {
+        //     title: 'Father',
+        //     dataIndex: 'fatherName',
+        // },
+        // {
+        //     title: 'Contact number',
+        //     dataIndex: 'contactNo',
+        // },
         {
             title: 'Fee / month',
             dataIndex: 'feePerMonth',
@@ -46,6 +53,7 @@ const StudentsTable = ({
       ];
   return (
     <Table
+        rowKey={"grno"}
         columns={columns}
         dataSource={data}
         pagination={false}
