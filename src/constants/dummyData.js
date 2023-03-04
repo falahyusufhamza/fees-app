132fe008601eb82b0e0a4244793303c75c751978
falahyusufhamza <falahyusufhamza@gmail.com>
2023-03-04 22:27:05 +0400

    Added table footer with actions, SPP actions, Edit student data

--- src/constants/dummyData.js
+++ src/constants/dummyData.js
@@ -9,5 +9,24 @@ export const studentsData = [
       fatherName: "Yusuf",
       contactNo: "99523224",
       feePerMonth: 10, 
+      gender: "Male",
+      siblings: [{
+        grno: 2,
+      }]
+    },
+    {
+      grno: 2,
+      name: "Mohammed Fasil Yusuf Hamza",
+      school: "Indian School Ghubra",
+      class: "12-B",
+      location: "Al khuwair",
+      busNumber: "15",
+      fatherName: "Yusuf",
+      contactNo: "99523224",
+      feePerMonth: 10, 
+      gender: "Male",
+      siblings: [{
+        grno: 1,
+      }]
     },
   ];
