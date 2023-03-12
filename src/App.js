d9856b42b9eed0a72e41d32396a3fa9f7ea05d43
falahyusufhamza <falahyusufhamza@gmail.com>
2023-03-12 15:36:35 +0400

    Routing + Login page UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import AppLayout from './components/common/Layout';
 import Dashboard from './pages/Dashboard';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import 'antd/dist/reset.css';
+
+const LayoutWrapper = ({element}) => {
+  return <div className='App'>
+      <AppLayout>
+      {element}
+    </AppLayout>
+    </div>
+}
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/dashboard",
+      element: <LayoutWrapper element={<Dashboard/>}/>
+    },
+    {
+      path: "/login",
+      element: <LoginPage/>
+    }
+  ])
   return (
-    <div className="App">
-      <AppLayout>
-        <Dashboard/>
-      </AppLayout>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
